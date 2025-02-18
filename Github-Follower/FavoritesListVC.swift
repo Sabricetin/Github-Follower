2d8de569081a17cb37fd1b5d9fbfb2d6337489db
Sabricetin <sabriyazilim@gmail.com>
2025-02-18 22:44:34 +0300

    Navigation Control
    Navigation Control added

---
+++ Github-Follower/FavoritesListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesListVC.swift
+//  Github-Follower
+//
+//  Created by Sabri Çetin on 18.02.2025.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+}
---
+++ Github-Follower/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  Github-Follower
+//
+//  Created by Sabri Çetin on 18.02.2025.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
