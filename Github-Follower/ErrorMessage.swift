c9a3e042591e35cac84eca85c635abcae812578e
Sabricetin <sabriyazilim@gmail.com>
2025-02-27 10:08:01 +0300

    Refactor Error Messages
    - Custom error handling added
    
    - GFButton bug fix
    
    - Used error.localizedDescription
    
    - Implemented enum raw values

---
+++ Github-Follower/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  Github-Follower
+//
+//  Created by Sabri Çetin on 27.02.2025.
+//
+
+import Foundation
+
+enum ErrorMessage : String {
+    case invalideUsername    = "This username created an invalid request. Please try again."
+    case unableToComplete    = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse     = "Invalid response from the server. Please try again"
+    case invalidData         = "The data received from the servver was invalid. Please try again."
+}
