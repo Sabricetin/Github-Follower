acdbd78e4179d7257f28ea900a56579f13875af0
Sabricetin <sabriyazilim@gmail.com>
2025-02-28 12:16:47 +0300

    UICollectionViewFlowLayout
    These were done in this code
    
    -UICollectionViewFlowLayout

--- Github-Follower/FollowerListVC.swift
+++ Github-Follower/FollowerListVC.swift
@@ -17,7 +17,7 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         configureCollectionView()
         configureViewController ()
-        getFollowers()  
+        getFollowers()
       
         
     }
@@ -33,11 +33,31 @@ class FollowerListVC: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPurple
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseId)
     }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        
+       
+        
+        let width                        = view.bounds.width
+        let padding : CGFloat            = 12
+        let minumumItemSpacing : CGFloat = 10
+        let availabelWidth               = width - (padding * 2) - (minumumItemSpacing * 2)
+        let itemWidth                    = availabelWidth / 3
+        
+        let flowLayout                   = UICollectionViewFlowLayout()
+        flowLayout.sectionInset          = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize              = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        
+        
+        return flowLayout   
+    }
+    
     
     func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
