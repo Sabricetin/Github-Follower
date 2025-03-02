b552fabcfd3504536f550d90d9fd58d0738db86a
Sabricetin <sabriyazilim@gmail.com>
2025-03-02 18:27:56 +0300

    Pagination of network calls
    These were done in this code
    
    -Pagination of network calls
    
    -Detecting the bottom of a UIScrollView

--- Github-Follower/FollowerListVC.swift
+++ Github-Follower/FollowerListVC.swift
@@ -13,6 +13,8 @@ class FollowerListVC: UIViewController {
     
     var username : String!
     var followers: [Follower] = []
+    var page = 1
+    var hasMoreFollowers = true
     var collectionView: UICollectionView!
     var dataSource : UICollectionViewDiffableDataSource<Section , Follower>!
     
@@ -20,7 +22,7 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         configureCollectionView()
         configureViewController ()
-        getFollowers()
+        getFollowers(username: username, page: page)
         configureDataSource()
         updateData()
         
@@ -39,6 +41,7 @@ class FollowerListVC: UIViewController {
     func configureCollectionView() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
+        collectionView.delegate = self
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseId)
     }
@@ -46,14 +49,15 @@ class FollowerListVC: UIViewController {
 
     
     
-    func getFollowers() {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+    func getFollowers(username: String , page: Int) {
+        NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             
             guard let self = self else {return}
             
             switch result {
             case.success(let followers):
-                self.followers = followers
+                if followers.count < 100 { self.hasMoreFollowers = false }
+                self.followers.append(contentsOf: followers)
                 self.updateData()
 
 
@@ -81,5 +85,22 @@ class FollowerListVC: UIViewController {
         
     }
 
+    
+}
+
+
+extension  FollowerListVC : UICollectionViewDelegate {
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+            
+        if offsetY > contentHeight - height {
+            guard hasMoreFollowers else { return }
+            page += 1
+            getFollowers(username: username, page: page)
+        }
+    }
     
 }
