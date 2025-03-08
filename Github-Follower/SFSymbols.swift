aa0263cbf5c22091cfba9f402eb1c65de03b34f3
Sabricetin <sabriyazilim@gmail.com>
2025-03-08 17:54:14 +0300

    Item Info View
    İn This Code These Are Done
    
    -Item Information View
    
    -Enumerations Key Expressions

--- Github-Follower/SFSymbols.swift
+++ Github-Follower/SFSymbols.swift
@@ -8,5 +8,9 @@
 import Foundation
 
 enum SFSymbols   {
-    static let location = "mappin.and.ellipse"
+    static let location       = "mappin.and.ellipse"
+    static let repos          = "folder.fill"
+    static let gists          = "text.alignleft"
+    static let followers      = "heart.square.fill"
+    static let following      = "person.2.fill"
 }
